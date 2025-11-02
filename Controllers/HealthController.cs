1870df5971b74c1468243c0ba3e5116285be69d9
Scott Harris <scotth519@gmail.com>
2025-11-01 18:23:54 -0700

    Conflicting changes with HealthController Updated

--- Controllers/HealthController.cs
+++ Controllers/HealthController.cs
@@ -41,29 +41,4 @@ namespace MyGuitarShop.Api.Controllers
             }
         }
     }
-
-    /*[Route("api/[controller]")]
-    [ApiController]
-    public class ProductsController(
-        ILogger<ProductsController> logger,
-        ProductRepo repo)
-        : ControllerBase
-    {
-        [HttpGet]
-        public async Task<IActionResult> GetAllAsync()
-        {
-            try
-            {
-                var products = await repo.GetAllProductsAsync();
-
-                return Ok(products.Select(p => p.ProductName));
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, "Error fetching Products");
-
-                return StatusCode(StatusCodes.Status500InternalServerError, "Internal server error");
-            }
-        }
-    }*/
 }
